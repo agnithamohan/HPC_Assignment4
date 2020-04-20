95e5c847d41d9f7fc18919326c7a884411f247af
agnithamohan <agnitha.mohan@gmail.com>
2020-04-20 12:38:44 -0400

    minor

--- 2D-jacobi.cu
+++ 2D-jacobi.cu
@@ -7,17 +7,6 @@
 
 using namespace std; 
 
-__device__ double atomicAddDouble(double* address, double val)
-{
-unsigned long long int* address_as_ull = (unsigned long long int*)address;
-unsigned long long int old = *address_as_ull, assumed;
-do {
-assumed = old;
-old = atomicCAS(address_as_ull, assumed,
-__double_as_longlong(val + __longlong_as_double(assumed)));
-} while (assumed != old);
-return __longlong_as_double(old);
-}
 
 __device__ double norm_calc_device;  
 __global__ void JacobiKernel(double *u, double *u_new, int N, double h_sq) {
